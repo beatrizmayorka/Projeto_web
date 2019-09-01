60dfe04bd62e1c24d6cd1afb73e456f162902e96
Beatriz Mayorka <mayorka_beatriz@hotmail.com.br>
2019-09-01 19:52:33 -0300

    Criado um arquivo projectsService.js e feito algumas alterações na página.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,40 @@
 var express = require('express');
 var router = express.Router();
 var postsService = require('../services/postsService');
+var projectsService = require('../services/projectsService');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var posts = postsService.getPosts();
+  var projects = projectsService.getProjects();
 
-  res.render('index', { title: 'Blog', posts: posts });
+  res.render('index', { title: 'Blog', posts: posts, projects: projects});
 });
+
+
+  router.get('/posts/:postId', function(req, res, next) {
+    var postId = req.params.postId;
+    var posts = postsService.getPosts();
+    var post = posts.filter((post) => post.id == postId) [0];
+
+    res.render('post', { title: post.title, post: post });
+
+  });
+
+
+  router.get('/', function(req, res, next) {
+    var projects = projectsService.getProjects();
+  
+    res.render('projects', { title: 'Blog', projects: projects });
+  });
+  
+    router.get('/project/:projectId', function(req, res, next) {
+      var projectId = req.params.projectId;
+      var projects = projectsService.getProjects();
+      var project = projects.filter((project) => project.id == projectId) [0];
+  
+      res.render('project', { title: project.title, project: project });
+  
+    });
 
 module.exports = router;
--- services/postsService.js
+++ services/postsService.js
@@ -1,18 +1,28 @@
 var getPosts = function() {
     var posts = [
         {
+            id: 1,
             title: 'Post 1',
             image: 'post1.jpg',
-            description: 'Meu primeiro post',
-            body: 'Meu primeiro post bla bla'
+            description: 'Cabine de comando do avião',
+            body: 'A cabine de um Boeing é ocupada pelo comandante e o copiloto. As atribuições são as mesmas, mas o comandante é o responsável pela aeronave e cabe a ele decolar e pousar. As demais tarefas são divididas. Normalmente, porém, o copiloto se comunica com os órgãos de controle, preenche o plano de voo e checa os procedimentos.'
         },
 
         {
+            id: 2,
             title: 'Post 2',
             image: 'post2.jpg',
-            description: 'Meu segundo post',
-            body: 'Meu segundo post bla bla'
+            description: 'Azul tem trimestre recorde e esperança de conseguir tudo em Congonhas',
+            body: 'Companhia aérea encerrou segundo trimestre com alta acima das expectativas, em mescla de eficiência na própria operação e sorte com a saída da Avianca'
         },
+
+        {
+            id: 3,
+            title: 'Post 3',
+            image: 'post3.jpg',
+            description: 'Aeroporto',
+            body: 'Um Aeroporto é um aeródromo público dotado de instalações e facilidades para apoio de operações de aeronaves, embarque e desembarque de pessoas e/ou coisas. Aeroportos precisam ser de fácil acesso a estradas, para o transporte de passageiros, trabalhadores e carga do aeroporto a outras cidades.'
+        }
     ];
 
     return posts;
