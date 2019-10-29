9c2afe75bfacb62d912dcbe4ed4bd81452647a5c
Beatriz Mayorka <mayorka_beatriz@hotmail.com.br>
2019-10-29 11:46:14 -0300

    Feito alterações em testimonialsService.js e posts.json

--- services/testimonialsService.js
+++ services/testimonialsService.js
@@ -2,17 +2,31 @@ var getTestimonials = function() {
     var testimonials = [
       {
         id: 1,
-        name: 'Fulano de tal',
-        company: 'Catolica SC',
-        title: 'Cara da TI',
-        testimony: 'Eu gosto muito de javascript'
+        name: 'Derick',
+        company: 'Oracle',
+        title: 'Software Engineer',
+        testimony: 'I love my company!!'
       },
       {
         id: 2,
-        name: 'Joaozinho',
-        company: 'Catolica SC',
-        title: 'Cara da Secretaria',
-        testimony: 'Eu tb gosto muito de javascript'
+        name: 'Maria Fernanda',
+        company: 'Facebook',
+        title: 'Systems Analyst',
+        testimony: 'I love my job!!'
+      },
+      {
+        id: 3,
+        name: 'Zayn',
+        company: 'Google',
+        title: 'Information Architect',
+        testimony: 'I am professionally accomplished!!'
+      },
+      {
+        id: 4,
+        name: 'Laura',
+        company: 'IBM - International Business Machines Corporation',
+        title: 'Software Architect',
+        testimony: 'I love Java!!'
       }
     ];
   
