a994ec3ca28d1792e3532a1ddb7c5f788cfc94a8
BEATRIZ MAYORKA DE AGUIAR <beatriz.aguiar@catolicasc.edu.br>
2019-10-28 21:32:19 -0300

    Adicionado middlewares, login, testimonials e feito algumas alterações.

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var testimonialsRouter = require('./routes/testimonials');
+var adminPostsRouter = require('./routes/admin/posts');
+var authRouter = require('./routes/auth');
+var verifyAuth = require('./middlewares/authMiddleware');
 
 var app = express();
+
+app.set('trust proxy', 1);
+app.use(session({
+  secret: 'mySecret123',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +33,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/auth', authRouter);
+app.use('/admin/posts', [verifyAuth],  adminPostsRouter);
 app.use('/users', usersRouter);
+app.use('/testimonials', testimonialsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,57 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var postsService = require('../services/postsService');
-var projectsService = require('../services/projectsService');
-var noticiasService = require('../services/noticiasService');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var posts = postsService.getPosts();
-  var projects = projectsService.getProjects();
-  var noticias = noticiasService.getNoticias();
 
-  res.render('index', { title: 'Blog', posts: posts, projects: projects, noticias: noticias});
+  res.render('index', { title: 'Blog', posts: posts });
 });
 
+router.get('/posts/:postId', function(req, res, next) {
+  var postId = req.params.postId;
 
-  router.get('/posts/:postId', function(req, res, next) {
-    var postId = req.params.postId;
-    var posts = postsService.getPosts();
-    var post = posts.filter((post) => post.id == postId) [0];
+  var posts = postsService.getPosts();
 
-    res.render('post', { title: post.title, post: post });
+  var post = posts.filter((post) => post.id == postId)[0];
 
-  });
+  res.render('post', { title: post.title, post: post });
 
-
-  router.get('/project', function(req, res, next) {
-    var projects = projectsService.getProjects();
-  
-    res.render('projects', { title: 'Projeto', projects: projects });
-  });
-  
-    router.get('/project/:projectId', function(req, res, next) {
-      var projectId = req.params.projectId;
-      var projects = projectsService.getProjects();
-      var project = projects.filter((project) => project.id == projectId) [0];
-  
-      res.render('project', { title: project.title, project: project });
-  
-    });
-
-    router.get('/noticias', function(req, res, next) {
-      var noticias = noticiasService.getNoticias();
-    
-      res.render('noticias', { title: 'Notícias', noticias: noticias });
-    });
-    
-      router.get('/noticia/:noticiaId', function(req, res, next) {
-        var noticiaId = req.params.noticiaId;
-        var noticias = noticiasService.getNoticias();
-        var noticia = noticias.filter((noticia) => noticia.id == noticiaId) [0];
-    
-        res.render('noticia', { title: noticia.title, noticia: noticia });
-    
-      });
+});
 
 module.exports = router;
